442d5300cdfecbf8664b38cbefdb55a15c810311
devangivadariya <devangivadariya1996@gmail.com>
2022-01-28 16:17:26 +0530

    Product list

---
+++ app/Http/Controllers/Api/ProductlistController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Product;
+
+class ProductlistController extends Controller
+{
+    public function getAllProducts() {
+      $product = Product::get()->toJson(JSON_PRETTY_PRINT);
+      return response($product, 200);
+    }
+}
